cdbef3104bce011fd44a9687644f223736be3b9a
GitRepo <cllafortune22@students.desu.edu>
2022-12-11 20:30:33 -0500

    yep

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -8,27 +8,40 @@
 
 public class Recitation7
 {
-    // define your constants after this comment
+    public static void main(String[] args){
 
-
+    }
     public static String leftRightTriangle(int height)
     {
-        String str = "";
-        //add your code here
-        return str;
+        StringBuilder str = new StringBuilder();
+        for (int row = 0; row <= height; row++){
+            str.append("*".repeat(row + 1));
+            str.append("\n");
+        }
+        return str.toString();
     }
 
     public static String rightRightTriangle(int height)
     {
-        String str = "";
-        //add your code here
-        return str;
+        StringBuilder str = new StringBuilder();
+        for(int row = 0; row <= height; row++){
+            str.append(" ".repeat(height - row - 1));
+            str.append("*" .repeat(row + 1));
+        }
+        return str.toString();
     }
 
     public static String circle(int radius)
     {
-        String str = "";
-        //add your code here
-        return str;
+        StringBuilder str = new StringBuilder();
+        for(int row = radius;  row >= - radius - 1; row--){
+            int verse = (int) Math.sqrt (radius * radius - row * row);
+            int x = (radius * 2 - (verse * 2));
+            if (x != radius * 2){
+                str.append(" ".repeat(Math.max(0, x /2 )));
+            }
+
+        }
+        return str.toString();
     }
 }
